52796e88bada36752ad12ade1a3ddf18761170f4
Erik Alonso <erikeah@protonmail.com>
2025-05-30 23:57:44 +0100

    chore: nixpkgs overlay

--- flake.nix
+++ flake.nix
@@ -8,18 +8,24 @@
 
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [  ];
+      imports = [
+          inputs.flake-parts.flakeModules.easyOverlay
+      ];
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       perSystem = { config, self', inputs', pkgs, system, ... }: {
+        overlayAttrs = {
+            inherit (config.packages) kak-xtra-rc;
+        };
         packages.default = pkgs.rustPlatform.buildRustPackage {
             name = "kak-xtra-rc";
             version = "0.1.0";
             src = ./.;
             cargoHash = "sha256-3Zuc7v/jHqY2bOc2vUeqruVprM2dFYLagmr/2e9EBbs=";
         };
+        packages.kak-xtra-rc = config.packages.default;
         devShells.default = with pkgs; mkShell {
             packages = [ rust-analyzer rustfmt ];
-            inputsFrom = [ config.packages.default ];
+            inputsFrom = [ config.packages.kak-xtra-rc ];
         };
       };
       flake = { };
